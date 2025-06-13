eb315c7212f8fa2f1a333a201bdcd102a697ef0b
ShubhamTechExplorer99 <shubhamjadhav0119@gmail.com>
2025-06-13 15:49:22 +0530

    Add test case TC_007_SampleTest

---
+++ src/test/java/com/internet_Banking_Projects_testCases/TC_007_SampleTest.java
@@ -0,0 +1,10 @@
+package com.internet_Banking_Projects_testCases;
+
+public class TC_007_SampleTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Extra added class");
+	}
+
+}
